7ad3d23ebed31139670edb5cd3345bd0670ba47f
Julien Prugne <julien@webeleon.dev>
2021-09-28 17:41:59 +0200

    test: add responsive tests

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -1,10 +1,31 @@
 describe('Index', () => {
-  it('Shoud load', () => {
-    cy.visit('/');
-    cy.get(`[data-cy='timer']`).should('be.visible');
-    cy.get(`[data-cy='open-dg']`).should('be.visible');
-    cy.get(`[data-cy='closed-dg']`).should('not.exist');
-    cy.get(`[data-cy='start']`).should('be.visible');
-    cy.get(`[data-cy='cancel']`).should('not.exist');
-  });
+  describe('responsive loads', () => {
+    function responsiveTest(target) {
+      cy.viewport(target, 'portrait');
+      cy.visit('/');
+      cy.get(`[data-cy='timer']`).should('be.visible');
+      cy.get(`[data-cy='open-dg']`).should('be.visible');
+      cy.get(`[data-cy='closed-dg']`).should('not.exist');
+      cy.get(`[data-cy='start']`).should('be.visible');
+      cy.get(`[data-cy='cancel']`).should('not.exist');
+
+      cy.viewport(target, 'landscape');
+      cy.visit('/');
+      cy.get(`[data-cy='timer']`).should('be.visible');
+      cy.get(`[data-cy='open-dg']`).should('be.visible');
+      cy.get(`[data-cy='closed-dg']`).should('not.exist');
+      cy.get(`[data-cy='start']`).should('be.visible');
+      cy.get(`[data-cy='cancel']`).should('not.exist');
+    }
+    it('iphone 6', () => {
+      responsiveTest('iphone-6')
+    });
+    it('macbook 13', () => {
+      responsiveTest('macbook-13')
+    })
+    it('macbook 16', () => {
+      responsiveTest('macbook-16')
+    })
+  })
+
 });
