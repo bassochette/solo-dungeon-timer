46d97b2ed156635fceb0d4bd59953290ecce6116
Julien Prugne <julien@webeleon.dev>
2021-09-28 17:49:06 +0200

    test: trial on landscape/portrait on iphone 6 target

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -1,7 +1,8 @@
 describe('Index', () => {
   describe('responsive loads', () => {
-    function responsiveTest(target) {
-      cy.viewport(target, 'portrait');
+
+    it('iphone 6 - portrait', () => {
+      cy.viewport("iphone-6", 'portrait');
       cy.visit('/');
       cy.get(`[data-cy='timer']`).should('be.visible');
       cy.get(`[data-cy='open-dg']`).should('be.visible');
@@ -9,23 +10,18 @@ describe('Index', () => {
       cy.get(`[data-cy='start']`).should('be.visible');
       cy.get(`[data-cy='cancel']`).should('not.exist');
 
-      cy.viewport(target, 'landscape');
+    });
+
+    it('iphone 6 - landscape', () => {
+      cy.viewport("iphone-6", 'landscape');
       cy.visit('/');
       cy.get(`[data-cy='timer']`).should('be.visible');
       cy.get(`[data-cy='open-dg']`).should('be.visible');
       cy.get(`[data-cy='closed-dg']`).should('not.exist');
       cy.get(`[data-cy='start']`).should('be.visible');
       cy.get(`[data-cy='cancel']`).should('not.exist');
-    }
-    it('iphone 6', () => {
-      responsiveTest('iphone-6')
-    });
-    it('macbook 13', () => {
-      responsiveTest('macbook-13')
     })
-    it('macbook 16', () => {
-      responsiveTest('macbook-16')
-    })
+
   })
 
 });
